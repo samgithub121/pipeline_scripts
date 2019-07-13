db4adb10f8d996d04fdb65c72cae028499d6c09a
samgithub121 <40270316+samgithub121@users.noreply.github.com>
2019-07-13 06:07:07 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,7 @@
 def info(message) {
-    echo "INFO: ${message}"
+    echo "INFO: This is an info section"
 }
 
 def warning(message) {
-    echo "WARNING: ${message}"
+    echo "WARNING: This is a warning section"
 }
